4a7bd5289791556158a7b4731dd8678bf5954da1
Weston Wei <weixuhong@gmail.com>
2023-03-21 01:07:03 +0800

    feat: 如果要使用nginx ingress 需要江node-type去掉

--- apply.sh
+++ apply.sh
@@ -13,7 +13,8 @@ kubectl apply -f ./helm/deploy-web-client.yaml
 # docker tag my-react-app localhost:5001/my-react-app:latest
 # docker push localhost:5001/my-react-app:latest
 
-#kubectl apply -f ./helm/nginx.yaml
+kubectl apply -f ./helm/nginx.yaml
 #kubectl apply -f ./helm/nginx-deployment.yaml
 #kubectl apply -f ./helm/nginx-service.yaml
-#kubectl apply -f ./helm/nginx-ingress.yaml
+kubectl apply -f ./helm/nginx-ingress-web.yaml
+kubectl apply -f ./helm/nginx-ingress-git.yaml
